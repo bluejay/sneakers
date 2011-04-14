7d89dc1ea7ffe0026dadc3e4d5e83c664e8ec5d6
Jay Palekar <jay.palekar@gmail.com>
2011-04-14 19:07:46 -0400

    Added the Sneakers method.
    This method will become the preferred way for people to create apps.
    
    Signed-off-by: Jay Palekar <jay.palekar@gmail.com>

--- lib/sneakers.rb
+++ lib/sneakers.rb
@@ -1,8 +1,19 @@
 module Sneakers
   ROOT = File.join(File.dirname(__FILE__), "sneakers")
 
+  require "rack"
+  require "fiber"
+  require "logger"
+
   autoload :Logger,       "#{ROOT}/logger.rb"
   autoload :Application,  "#{ROOT}/application.rb"
   autoload :Deferrable,   "#{ROOT}/deferrable.rb"
   autoload :Response,     "#{ROOT}/response.rb"
+end
+
+def Sneakers(&action)
+  return Proc.new do |env|
+    application = Sneakers::Application.new(env)
+    application.__call__(&action)
+  end
 end
